0a8afcf5e262de77e546d7f10839e6d7ee6a0d4b
Dan Eisner <deisner@g.ucla.edu>
2020-10-18 12:41:38 -0400

    Adding HiBenji to GitHub

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HiBenji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response = "";
+            Console.WriteLine("Welcome Benji!");
+            do
+            {
+                Console.Write("What do you want to say? ");
+                string what = Console.ReadLine();
+                Console.Write("How many times? ");
+                try
+                {
+                    int numberOfTimes = int.Parse(Console.ReadLine());
+                    for (int i = 1; i <= numberOfTimes; i++)
+                    {
+                        Console.WriteLine(i + ". " + what);
+                    }
+                    Console.WriteLine("Do you want to write something else?");
+                    response = Console.ReadLine().ToLowerInvariant();
+                    Console.Clear();
+                }
+                catch (Exception e)
+                { }
+            } while (response.Substring(0, 1) == "y");
+            
+        }
+    }
+}
